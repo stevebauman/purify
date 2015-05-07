a6e9286df11c1cc7a9bb9fe0d7a7df7373f5d98f
Steve Bauman <steven_bauman@outlook.com>
2015-05-07 12:25:29 -0400

    Formatting changes

--- src/Stevebauman/Purify/PurifyServiceProvider.php
+++ src/Stevebauman/Purify/PurifyServiceProvider.php
@@ -16,46 +16,31 @@ class PurifyServiceProvider extends ServiceProvider
     public static $packageConfigSeparator = '::';
 
     /**
-     * The laravel version number. This is
-     * used for the install commands.
+     * Indicates if loading of the provider is deferred.
      *
-     * @var int
+     * @var bool
      */
-    public static $laravelVersion = 4;
+    protected $defer = false;
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
-
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
         /*
          * If the package method exists, we're using Laravel 4, if not, we're on 5
          */
-        if (method_exists($this, 'package'))
-        {
+        if (method_exists($this, 'package')) {
             $this->package('stevebauman/purify');
-        } else
-        {
+        } else {
             /*
              * Set the proper configuration separator since
              * retrieving configuration values in packages
              * changed from '::' to '.'
              */
             $this::$packageConfigSeparator = '.';
-
-            /*
-             * Set the local inventory laravel version for easy checking
-             */
-            $this::$laravelVersion = 5;
 
             /*
              * Assign the configuration as publishable, and tag it as 'config'
@@ -66,19 +51,18 @@ class PurifyServiceProvider extends ServiceProvider
         }
 
         // Bind the new purify instance
-        $this->app->bind('purify', function($app)
-        {
+        $this->app->bind('purify', function ($app) {
             return new Purify($app['config']);
         });
-	}
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return ['purify'];
-	}
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['purify'];
+    }
 }
