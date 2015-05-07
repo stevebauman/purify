5043cc37580999d64541ae6c3a6479eff9f0b4e6
Steve Bauman <steven_bauman@outlook.com>
2015-05-07 11:16:32 -0400

    Added scrutinizer, whitelisted functional test case

--- tests/PurifyTest.php
+++ tests/PurifyTest.php
@@ -4,14 +4,28 @@ namespace Stevebauman\Purify\Tests;
 
 class PurifyTest extends FunctionalTestCase
 {
+    public $testInput = '<script>alert("Harmful Script");</script> <p style="a style" class="a-different-class">Test</p>';
+
     public function testClean()
     {
-        $input = '<script>alert("Harmful Script");</script> <p style="a style" class="a-different-class">Test</p>';
-
-        $cleaned = $this->purifier->clean($input);
+        $cleaned = $this->purifier->clean($this->testInput);
 
         $expected = '<p class="a-different-class">Test</p>';
 
         $this->assertEquals($expected, $cleaned);
+    }
+
+    public function testCleanArray()
+    {
+        $cleaned = $this->purifier->clean([$this->testInput, $this->testInput]);
+
+        $expected = ['<p class="a-different-class">Test</p>', '<p class="a-different-class">Test</p>'];
+
+        $this->assertEquals($expected, $cleaned);
+    }
+
+    public function testCleanMergeConfig()
+    {
+
     }
 }
