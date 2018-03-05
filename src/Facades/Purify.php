2e699ea77f65dca74d5f2f8e7dfcd61dd413b49c
Steve Bauman <steven_bauman@outlook.com>
2018-03-05 12:41:32 -0500

    Added PHPStorm method doc

--- src/Facades/Purify.php
+++ src/Facades/Purify.php
@@ -4,6 +4,11 @@ namespace Stevebauman\Purify\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * Class Purify
+ *
+ * @method static clean($input, array $config = null)
+ */
 class Purify extends Facade
 {
     /**
