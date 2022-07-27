fb2e9cda6a422f3203fbc0534ca761976ad0e2fb
Propaganistas <Propaganistas@users.noreply.github.com>
2022-07-27 16:54:05 +0200

    Update purify.php

--- config/purify.php
+++ config/purify.php
@@ -38,7 +38,6 @@ return [
     'configs' => [
         'default' => [
             'Core.Encoding' => 'utf-8',
-            'Cache.SerializerPath' => storage_path('app/purify'),
             'HTML.Doctype' => 'XHTML 1.0 Strict',
             'HTML.Allowed' => 'h1,h2,h3,h4,h5,h6,b,strong,i,em,a[href|title],ul,ol,li,p[style],br,span,img[width|height|alt|src]',
             'HTML.ForbiddenElements' => '',
