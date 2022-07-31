1ef5dd9ad2bba02a6a5a1625e722553197202181
Propaganistas <Propaganistas@users.noreply.github.com>
2022-07-31 09:41:03 +0200

    Add Eloquent casts

---
+++ tests/CastsTest.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Stevebauman\Purify\Tests;
+
+use HTMLPurifier_HTMLDefinition;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\File;
+use Stevebauman\Purify\Casts\PurifyHtmlOnGet;
+use Stevebauman\Purify\Casts\PurifyHtmlOnSet;
+use Stevebauman\Purify\Definitions\Definition;
+use Stevebauman\Purify\Facades\Purify;
+use Stevebauman\Purify\PurifyServiceProvider;
+
+class CastsTest extends TestCase
+{
+    public $testInput = '<script>alert("Harmful Script");</script><p style="a {color: #0000ff;}" class="a-different-class">Test<span>bar</span></p>';
+
+    public function test_purifies_on_get_with_default_config()
+    {
+        $this->app['config']->set('purify.configs.default', [
+            'HTML.Allowed' => 'p',
+        ]);
+
+        $model = new PurifyingDefaultOnGetModel();
+        $model->body = $this->testInput;
+
+        $this->assertEquals($this->testInput, $model->getAttributes()['body']);
+        $this->assertEquals('<p>Testbar</p>', $model->body);
+    }
+
+    public function test_purifies_on_get_with_custom_config()
+    {
+        $this->app['config']->set('purify.configs.foo', [
+            'HTML.Allowed' => 'p,span',
+        ]);
+
+        $model = new PurifyingFooOnGetModel();
+        $model->body = $this->testInput;
+
+        $this->assertEquals($this->testInput, $model->getAttributes()['body']);
+        $this->assertEquals('<p>Test<span>bar</span></p>', $model->body);
+    }
+
+    public function test_purifies_on_set_with_default_config()
+    {
+        $this->app['config']->set('purify.configs.default', [
+            'HTML.Allowed' => 'p',
+        ]);
+
+        $model = new PurifyingDefaultOnSetModel();
+        $model->body = $this->testInput;
+
+        $this->assertEquals('<p>Testbar</p>', $model->getAttributes()['body']);
+    }
+
+    public function test_purifies_on_set_with_custom_config()
+    {
+        $this->app['config']->set('purify.configs.foo', [
+            'HTML.Allowed' => 'p,span',
+        ]);
+
+        $model = new PurifyingFooOnSetModel();
+        $model->body = $this->testInput;
+
+        $this->assertEquals('<p>Test<span>bar</span></p>', $model->getAttributes()['body']);
+    }
+}
+
+class PurifyingDefaultOnGetModel extends Model
+{
+    protected $casts = [
+        'body' => PurifyHtmlOnGet::class,
+    ];
+}
+
+class PurifyingFooOnGetModel extends Model
+{
+    protected $casts = [
+        'body' => PurifyHtmlOnGet::class.':foo',
+    ];
+}
+
+
+class PurifyingDefaultOnSetModel extends Model
+{
+    protected $casts = [
+        'body' => PurifyHtmlOnSet::class,
+    ];
+}
+
+class PurifyingFooOnSetModel extends Model
+{
+    protected $casts = [
+        'body' => PurifyHtmlOnSet::class.':foo',
+    ];
+}
+
