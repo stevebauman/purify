1a42bfbd4007011b06c06416b0ec02c7d57e9d12
Steve Bauman <steven_bauman@outlook.com>
2023-02-05 17:30:10 -0500

    Update PurifyServiceProvider.php

--- src/PurifyServiceProvider.php
+++ src/PurifyServiceProvider.php
@@ -22,8 +22,8 @@ class PurifyServiceProvider extends ServiceProvider
         $this->app->singleton('purify', function ($app) {
             if ($cache = config('purify.serializer.cache')) {
                 // We must autoload the cache manually due to an issue
-                //with the root HTMLPurifier repository. Once this
-                // issue has been resolve, we may remove this.
+                // with the root HTMLPurifier repository. Once this
+                // issue has been resolved, we may remove this.
                 spl_autoload_call($cache);
 
                 HTMLPurifier_DefinitionCacheFactory::instance()->register($cache, $cache);
