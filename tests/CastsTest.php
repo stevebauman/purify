116a976a435cf6a9b45097dab5f2da4d9e2c0fe5
Propaganistas <Propaganistas@users.noreply.github.com>
2023-04-06 08:24:10 +0200

    add tests

--- tests/CastsTest.php
+++ tests/CastsTest.php
@@ -35,6 +35,14 @@ class CastsTest extends TestCase
         $this->assertEquals($this->testInput, $model->getAttributes()['body']);
         $this->assertEquals('<p>Test<span>bar</span></p>', $model->body);
     }
+    
+    public function test_returns_null_on_get_when_value_is_null()
+    {
+        $model = new PurifyingDefaultOnGetModel();
+        $model->body = null;
+
+        $this->assertNull($model->body);
+    }
 
     public function test_purifies_on_set_with_default_config()
     {
@@ -58,6 +66,14 @@ class CastsTest extends TestCase
         $model->body = $this->testInput;
 
         $this->assertEquals('<p>Test<span>bar</span></p>', $model->getAttributes()['body']);
+    }
+    
+    public function test_sets_null_on_set_when_value_is_null()
+    {
+        $model = new PurifyingDefaultOnSetModel();
+        $model->body = null;
+
+        $this->assertNull($model->getAttributes()['body']);
     }
 }
 
