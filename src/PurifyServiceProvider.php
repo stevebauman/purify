c245c337060d58039426816b9bb4d57ab3182c0e
Steve Bauman <steven_bauman@outlook.com>
2023-11-28 14:12:28 -0500

    Update to HTMLPurifier v4.17 to remove spl_autoload_call use

--- src/PurifyServiceProvider.php
+++ src/PurifyServiceProvider.php
@@ -21,12 +21,6 @@ class PurifyServiceProvider extends ServiceProvider
 
         $this->app->singleton('purify', function ($app) {
             if ($cache = config('purify.serializer.cache')) {
-                // We must autoload the cache manually due to an issue
-                // with the root HTMLPurifier repository. Once this
-                // issue has been resolved, we may remove this.
-                // See: https://github.com/ezyang/htmlpurifier/pull/364
-                spl_autoload_call($cache);
-
                 HTMLPurifier_DefinitionCacheFactory::instance()->register($cache, $cache);
             }
 
